8426d499edafebaa01dbc078f3427b8d3e41aa46
mikhlas9 <mohammadikhlas99@gmail.com>
2025-09-06 17:29:00 +0530

    upd

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://e-commerce-tau-rose.vercel.app',
   credentials: true
 }));
 app.use(express.json());
